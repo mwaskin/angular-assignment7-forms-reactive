762a609b0fc42d2053e9149e287e9294775bdba3
Matthew Waskin <matthew@waskin.com>
2023-09-14 14:51:20 -0400

    assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, resolveForwardRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'assignment7-forms-reactive';
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  statusOptions = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit(): void {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        Validators.required,
+        this.nameValidator
+      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      status: new FormControl(null),
+    });
+  }
+
+  nameValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value.toLowerCase() === 'test') {
+          resolve({ nameIsInvalid: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
 }
